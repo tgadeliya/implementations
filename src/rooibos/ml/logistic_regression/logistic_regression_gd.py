9b837e249d8cc3594206f2673ab3be9d29cdb443
Tsimur Hadeliya <tgadeliya@gmail.com>
2025-05-09 18:05:37 +0300

    Fix log reg

--- src/rooibos/ml/logistic_regression/logistic_regression_gd.py
+++ src/rooibos/ml/logistic_regression/logistic_regression_gd.py
@@ -2,31 +2,31 @@ from math import exp
 
 
 class LogisticRegressionGD:
-    def __init__(self, lr, n_epochs):
+    def __init__(self, lr: float, n_epochs: int) -> None:
         self.lr = lr
         self.n_epochs = n_epochs
 
         self.W: list[float]
         self.b: float
 
-    def dot(self, x, y):
+    def dot(self, x: list[float], y: list[float]) -> float:
         return sum([i * j for i, j in zip(x, y)])
 
-    def sigmoid(self, x):
+    def sigmoid(self, x: float) -> float:
         if x < 0:
             return 1 - 1 / (1 + exp(x))
         return 1 / (1 + exp(-x))
 
-    def _init_weights(self, n_feat):
+    def _init_weights(self, n_feat: int) -> None:
         self.W = [0.0] * n_feat
         self.b = 0.0
 
-    def _update_weights(self, gw, gb):
+    def _update_weights(self, gw: list[float], gb: float) -> None:
         for i in range(len(self.W)):
             self.W[i] -= self.lr * gw[i]
         self.b -= self.lr * gb
 
-    def train(self, X, y) -> dict[str, list[float] | float]:
+    def train(self, X: list[list[float]], y: list[float]) -> dict[str, list[float] | float]:
         n_feat = len(X[0])
         self._init_weights(n_feat)
 
@@ -36,21 +36,24 @@ class LogisticRegressionGD:
 
         return {"weight": self.W, "bias": self.b}
 
-    def train_one_epoch(self, X, y):
+    def train_one_epoch(self, X: list[list[float]], y: list[float]) -> None:
         for i in range(len(X)):
             grad_w, grad_b = self.training_step(X[i], y[i])
             self._update_weights(grad_w, grad_b)
 
-    def training_step(self, x, y):
-        grad_w = []
+    def training_step(self, x: list[float], y: float) -> tuple[list[float], float]:
+        grad_w: list[float] = []
         prod = y * (self.dot(x, self.W) + self.b)
         for i in range(len(x)):
             res = (1 - self.sigmoid(prod)) * -y * x[i]
             grad_w.append(res)
 
         grad_b = (1 - self.sigmoid(prod)) * -y
-
         return grad_w, grad_b
 
-    def predict():
-        pass
+    def predict(self, X: list[list[float]]) -> list[float]:
+        return [self.predict_example(x) for x in X]
+
+    def predict_example(self, x: list[float]) -> float:
+        prod: float = self.dot(x, self.W) + self.b
+        return self.sigmoid(prod)
